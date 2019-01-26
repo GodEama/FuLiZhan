f02f2c23142de7a3514e5ee6b22f4c202f68ff8c
Guo Xiaojun <842956481@qq.com>
2019-01-26 14:53:15 +0800

    嗯

---
+++ FuliZhan/Classes/Model/kyj.swift
@@ -0,0 +1,76 @@
+//
+//  kyj.swift
+//  FuliZhan
+//
+//  Created by 123 on 2019/1/26.
+//  Copyright © 2019 jingying. All rights reserved.
+//
+
+import Foundation
+import HandyJSON
+
+class Userinfo    : NSObject {
+    var id: String!
+    var user_nicename: String!
+    var avatar: String!
+    var coin: String!
+    var avatar_thumb: String!
+    var sex: String!
+    var signature: String!
+    var province: String!
+    var city: String!
+    var birthday: String!
+    var age: String!
+    var code: String!
+    var vip_endtime: String!
+    var praise: String!
+    var fans: String!
+    var follows: String!
+    var workVideos: String!
+    var likeVideos: String!
+}
+
+class Musicinfo    : NSObject {
+    var id: String!
+    var title: String!
+    var author: String!
+    var img_url: String!
+    var length: String!
+    var file_url: String!
+    var use_nums: String!
+    var music_format: String!
+}
+
+class InfoItem    : NSObject {
+    var id: String!
+    var uid: String!
+    var title: String!
+    var thumb: String!
+    var thumb_s: String!
+    var href: String!
+    var likes: String!
+    var views: String!
+    var comments: String!
+    var steps: String!
+    var shares: String!
+    var addtime: String!
+    var lat: String!
+    var lng: String!
+    var city: String!
+    var isdel: String!
+    var music_id: String!
+    var xiajia_reason: String!
+    var show_val: String!
+    var nopass_time: String!
+    var watch_ok: String!
+    var is_ad: String!
+    var ad_endtime: String!
+    var ad_url: String!
+    var ad_desc: String!
+    var orderno: String!
+    var datetime: String!
+    var islike: String!
+    var isattent: String!
+    var isstep: String!
+    var isdialect: String!
+}
